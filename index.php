3158458961c880d994738a6c8256e0f081d0c330
CassiGS <ccgall@gmail.com>
2018-11-04 18:29:32 -0800

    Add initial wordpress files

---
+++ index.php
@@ -0,0 +1,9 @@
+/*
+Theme Name: Jeff Shearer Marketing Theme
+Description: A custom theme for Jeff Shearer's portfolio site
+Author: Cassi Gallagher-Shearer
+Author URI: http://www.cassigallagher.com/
+Version: 1.0
+Tags: responsive
+
+*/
