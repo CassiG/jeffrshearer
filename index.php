c69585e527aa87bbdad11db85b43eab94b1d2ca3
CassiGS <ccgall@gmail.com>
2018-11-04 19:03:34 -0800

    Add additional wordpress styles

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-/*
-Theme Name: Jeff Shearer Marketing Theme
-Description: A custom theme for Jeff Shearer's portfolio site
-Author: Cassi Gallagher-Shearer
-Author URI: http://www.cassigallagher.com/
-Version: 1.0
-Tags: responsive
-
-*/
+<?php get_header(); ?>
+<?php get_footer(); ?>
